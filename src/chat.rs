714c0f9d7e10f10e9a1ff7350e0590fe5d464ef5
any <any@any>
2025-09-12 17:44:22 +0800

    unfinished

--- src/chat.rs
+++ src/chat.rs
@@ -1,15 +1,15 @@
 use crate::cli::response_render::{RenderConfig, ResponseRenderer};
 use crate::{
     config::{ModelConfig, PromptConfig},
-    log_debug
+    log_debug,
 };
 use async_openai::{
     Client,
     config::OpenAIConfig,
     error::OpenAIError,
     types::{
-        ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs,
-        CreateChatCompletionRequest, CreateChatCompletionRequestArgs,
+        ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequest,
+        CreateChatCompletionRequestArgs,
     },
 };
 use futures::StreamExt;
@@ -18,24 +18,43 @@ pub async fn completion(
     input: &str,
     model_config_name: String,
     model_config: &ModelConfig,
-    prompt_config_name:String,
+    prompt_config_name: String,
     prompt_config: &PromptConfig,
     pure: bool,
     disable_stream: bool,
     verbose: bool,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let _=verbose;
+    let _ = verbose;
     let client = create_client(&model_config);
     let model_name = model_config.model_name.as_ref().unwrap();
-    
+    // 创建渲染器配置
+    let config = RenderConfig {
+        pure,
+        model_config_name: model_config_name,
+        model_name: model_name.to_string(),
+        prompt_config_name,
+        type_speed: 50, // 50字/秒
+        disable_stream: disable_stream,
+    };
+    let mut errors = Vec::<OpenAIError>::new();
+    let renderer = ResponseRenderer::new();
+    let (message_tx, renderer_handler) = renderer.start_render(config);
+
     if disable_stream {
+        log_debug!("Start send chat request.");
         let response = client
             .chat()
             .create(create_request(input, &prompt_config, &model_config))
             .await?;
+        log_debug!("Received chat response.");
 
         if let Some(choice) = response.choices.first() {
-            println!("\n{}", choice.message.content.as_ref().unwrap());
+            if let Err(err) = message_tx
+                .send(choice.message.content.clone().unwrap_or(String::from("null")))
+                .await
+            {
+                eprintln!("send message failed: {}", err);
+            };
         }
     } else {
         let mut stream = client
@@ -43,17 +62,6 @@ pub async fn completion(
             .create_stream(create_request(input, &prompt_config, &model_config))
             .await?;
 
-        // 创建渲染器配置
-        let config = RenderConfig {
-            pure,
-            model_config_name: model_config_name,
-            model_name: model_name.to_string(),
-            prompt_config_name,
-            type_speed: 50, // 50字/秒
-        };
-        let mut errors = Vec::<OpenAIError>::new();
-        let renderer = ResponseRenderer::new();
-        let (message_tx, renderer_handler) = renderer.start_render(config);
         log_debug!("Start receive stream message.");
         while let Some(result) = stream.next().await {
             match result {
@@ -70,19 +78,16 @@ pub async fn completion(
             }
         }
         log_debug!("Exit receive stream message.");
-        drop(message_tx);
-        log_debug!("Drop Message Sender.");
-        let _=renderer_handler.await;
-        log_debug!("Response Render exit.");
-
-
-        if !errors.is_empty() {
-            for e in errors {
-                println!("Error happends: {}", e);
-            }
+    }
+    drop(message_tx);
+    log_debug!("Drop Message Sender.");
+    let _ = renderer_handler.await;
+    log_debug!("Response Render exit.");
+    if !errors.is_empty() {
+        for e in errors {
+            println!("Error happends: {}", e);
         }
     }
-
     Ok(())
 }
 
